d30d0d78215db2bef2c30039cc8f3a058a8d5b35
uwspstar <uwspstar@gmail.com>
2017-09-24 22:22:44 -0500

    example 1

--- FuntionalCSharp/FuntionalCSharp/Program.cs
+++ FuntionalCSharp/FuntionalCSharp/Program.cs
@@ -10,6 +10,20 @@ namespace FuntionalCSharp
     {
         static void Main(string[] args)
         {
+            Example_1();
+        }
+
+        public static void Example_1()
+        {
+            //7! = 7*6*5*4*3*2*1
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            IEnumerable<int> ints = Enumerable.Range(1, input);
+            int factorialNumber = ints.Aggregate((f, s) => f * s);
+
+            Console.WriteLine("{0}! is {1}", input, factorialNumber);
+
+            Console.ReadLine();
         }
     }
 }
