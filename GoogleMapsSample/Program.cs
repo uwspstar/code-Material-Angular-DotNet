ae98b115bb6fbde615f08f21cae19b662674b8b1
xing wang <uwspstar@gmail.com>
2017-09-28 10:44:56 -0500

    add google map with httpClient

---
+++ GoogleMapsSample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+
+namespace GoogleMapsSample
+{
+    /// <summary>
+    ///  Downloads a Redmond map from Google Maps, saves it as a local file and opens the default viewer.
+    /// </summary>
+    class Program
+    {
+        static string _address = "http://maps.googleapis.com/maps/api/staticmap?center=Austin,Tx&zoom=14&size=400x400&sensor=false";
+
+        static void Main(string[] args)
+        {
+            GetGoogleMap();
+
+            Console.WriteLine("Exit...");
+            Console.ReadLine();
+        }
+
+        static async void GetGoogleMap()
+        {
+            var client = new HttpClient(); 
+           
+            var response = await client.GetAsync(_address); 
+             
+            response.EnsureSuccessStatusCode();
+             
+            using (var fileStream = new FileStream("output.png", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                await response.Content.CopyToAsync(fileStream);
+            }
+
+            var process = new Process();
+            process.StartInfo.FileName = "output.png";
+            process.Start();
+        } 
+        
+    }
+}
---
+++ GoogleMapsSample/ReadMe.txt
@@ -0,0 +1,5 @@
+GoogleMapsSample
+----------------
+
+For more information about the samples, please see
+http://go.microsoft.com/fwlink/?LinkId=261487
