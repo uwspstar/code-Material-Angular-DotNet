6bc7f3b70be6dcb2fb7cfeb537e8a5e732d8ab4b
xing <xing@xings-MBP.attlocal.net>
2017-10-05 06:21:38 -0500

    no message

--- dotNetCoreAngular/demo1/Models/Make.cs
+++ dotNetCoreAngular/demo1/Models/Make.cs
@@ -2,6 +2,15 @@ namespace demo1.Models
 {
     public class Make
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+
+        public Make()
+        {
+            Models = new Collection<Model>();
+            
+        }
         
     }
 }
--- dotNetCoreAngular/demo1/Models/Model.cs
+++ dotNetCoreAngular/demo1/Models/Model.cs
@@ -2,6 +2,7 @@ namespace demo1.Models
 {
     public class Model
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
