9ff7e8b7ab4d07e0389311949017d3a0422d8266
xing <xing@xings-MBP.attlocal.net>
2017-10-12 06:09:42 -0500

    move to new place

---
+++ bootstrap4/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAWnr1QTQggAg8hHfw-ai_OdC2xFPDHXGs',
+    authDomain: 'jaxbot-3ca0f.firebaseapp.com',
+    databaseURL: 'https://jaxbot-3ca0f.firebaseio.com',
+    projectId: 'jaxbot-3ca0f',
+    storageBucket: '',
+    messagingSenderId: '744013134041'
+  }
+};
