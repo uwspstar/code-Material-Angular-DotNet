4ece168116e1ab52c73423960fd8b7365ff115f2
xing <xing@xings-MBP.attlocal.net>
2017-10-14 18:39:42 -0500

    no message

--- Angular4Complete/README.md
+++ Angular4Complete/README.md
@@ -18,17 +18,17 @@ $ ng g c header --dry-run
 $ ng g c header
 
 // create recipes component without Unit test
-$ ng g c receipes --spec false --dry-run
-$ ng g c receipes --spec false
+$ ng g c recipes --spec false --dry-run
+$ ng g c recipes --spec false
 
 // create recipe-list component without Unit test
-$ ng g c receipes/receipt-list --spec false
+$ ng g c recipes/recipe-list --spec false
 
 // create recipe-detail component without Unit test
-$ ng g c receipes/receipt-detail --spec false
+$ ng g c recipes/recipe-detail --spec false
 
 // create recipe-item component without Unit test
-$ ng g c receipes/receipt-list/recipt-item --spec false
+$ ng g c recipes/recipe-list/recipe-item --spec false
 
 // create shopping-list component without Unit test
 $ ng g c shopping-list --spec false
--- Angular4Complete/src/app/app.module.ts
+++ Angular4Complete/src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ReceipesComponent } from './receipes/receipes.component';
-import { ReceiptListComponent } from './receipes/receipt-list/receipt-list.component';
-import { ReceiptDetailComponent } from './receipes/receipt-detail/receipt-detail.component';
-import { ReciptItemComponent } from './receipes/receipt-list/recipt-item/recipt-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ReceipesComponent,
-    ReceiptListComponent,
-    ReceiptDetailComponent,
-    ReciptItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
