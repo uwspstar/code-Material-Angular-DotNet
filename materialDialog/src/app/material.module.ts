611b6a655e756b51f6644d24f84c4ecbc5cd24c1
xing <xing@xings-MBP.attlocal.net>
2017-10-22 06:23:09 -0500

    remove unused import

--- materialDialog/src/app/material.module.ts
+++ materialDialog/src/app/material.module.ts
@@ -1,7 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { MatDialogModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
