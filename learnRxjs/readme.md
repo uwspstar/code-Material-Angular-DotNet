535e696887e0eff4c77f6d90d360f6f4cfef355e
xing wang <uwspstar@gmail.com>
2017-11-08 05:25:09 -0600

    no message

---
+++ learnRxjs/readme.md
@@ -0,0 +1,3 @@
+- recommend to watch [Getting Started with Reactive Programming Using RxJS](https://app.pluralsight.com/library/courses/reactive-programming-rxjs-getting-started/table-of-contents)
+
+- recommend to read [RxJS Quick Start With 20 Practical Examples](https://angularfirebase.com/lessons/rxjs-quickstart-with-20-examples/) by Jeff Delaney
---
+++ learnRxjs/script.js
@@ -0,0 +1,29 @@
+// 1. Create Observables - Define the stream
+
+const observable = Rx.Observable.create(observer => {
+    observer.next('hello')
+    observer.next('world')
+})
+observable.subscribe(val => console.log(val))
+// hello
+// world
+
+// 2. create an observable from mouse clicks
+const clicks = Rx.Observable.fromEvent(document, 'click')
+clicks.subscribe(click => console.log(click))
+// click around the web page...
+// MouseEvent<data>
+// MouseEvent<data>
+
+
+// 3 .convert a promise to an observable with fromPromise()
+// You can also convert an observable right back to a promise using the toPromise() method.
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('resolved!')
+    }, 1000)
+});
+const obsvPromise = Rx.Observable.fromPromise(promise)
+obsvPromise.subscribe(result => console.log("result is :" + result))
+// wail 1 second...
+// resolved!
