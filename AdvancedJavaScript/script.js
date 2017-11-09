e2c95972631fdd5524ef012177e4ea04f2498038
xing wang <uwspstar@gmail.com>
2017-11-09 04:43:28 -0600

    no message

--- AdvancedJavaScript/script.js
+++ AdvancedJavaScript/script.js
@@ -1,1 +1,11 @@
-alert("hi");
+
+// Array
+var ar1 = Array(3);
+ar1[0] = "Xing";
+ar1[1] = true;
+ar1[2] = 88.88;
+
+var ar2 = ["xing", true, 888];
+//console.log(ar2.length);
+//Error : ar2.length();  // ar2.length is not a function
+console.log(` ar2.length = ${ar2.length}`);
